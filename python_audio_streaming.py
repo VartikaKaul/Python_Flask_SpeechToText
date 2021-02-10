c59a312d72a713342d12517d94bf243d9738bcfe
VartikaKaul <78537604+VartikaKaul@users.noreply.github.com>
2021-02-10 16:37:19 +0530

    Update python_audio_streaming.py

--- python_audio_streaming.py
+++ python_audio_streaming.py
@@ -132,4 +132,4 @@ def index():
 # </html>
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, threaded=True,port=5000)
+    app.run(host='0.0.0.0', debug=True, threaded=True,port=2203)
